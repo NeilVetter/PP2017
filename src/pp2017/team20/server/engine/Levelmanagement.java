a808ea1e8f66f977c0fc78d2b226682b303e4ccf
Hamidk <Hamidk@Hamid.Speedport_W_724V_01011603_05_032>
2017-07-12 14:49:59 +0200

    lvlmanagment/ Player/Lvl

--- src/pp2017/team20/server/engine/Levelmanagement.java
+++ src/pp2017/team20/server/engine/Levelmanagement.java
@@ -1,18 +1,30 @@
 package pp2017.team20.server.engine;
 
 import pp2017.team20.shared.*;
+
+import java.util.ArrayList;
+
 import pp2017.team20.server.map.*;
 
 //Neil Vetter 6021336
 //Diese Klasse erzeugt erst, und speichert dann das Level in einem Array
 
 public class Levelmanagement {
-
+	
 	// Erstellt ein Objekt vom Typ "Player"
 	public Player player;
-
+	
+	Player[] playerList;
+	Monster[] monsterList;
+	Healthpot[] healpotList;
+	public int[][] lvlMaze;
 	public int LevelID = -1;
-	public int size = 15;
+	public static int size;
+	public int lvlcount;
+	static int keyx;
+	static int keyy;
+	static int doorx;
+	static int doory;
 
 	public Levelmanagement(Player player) {
 		this.player = player;
@@ -30,15 +42,55 @@ public class Levelmanagement {
 		// Speicher im Spieler welches Level er zuletzt gespielt hat
 		// Lvlindex geht von 0,1,...
 		player.playerLvl = LevelID;
+		
 
 		// Speichert die Map beim Spieler, sodass Elemente darauf abgelesen
 		// werden kï¿½nnen
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
 				player.playerMap[i][j] = maze.Map[i][j];
+				
 			}
 		}
-
+		int playerID = 0;
+		int monsterID = 0;
+		int healpotID =0;
+		//Arrays definieren 
+		playerList = new Player[LevelID];
+		monsterList = new Monster[LevelID*3];
+		healpotList = new Healthpot[LevelID];
+		
+		for (int i=0; i<player.playerMap.length; i++){
+			for (int j=0;j<player.playerMap.length;j++){
+				if (player.playerMap[i][j]==2){
+					Player player = new Player(playerID ,i, j);
+					// wieso passt es nicht mit deinem Konstrucktor @neil
+					player.setPos(i,j);
+					playerList[playerID]= player;
+					playerID++;
+				}
+				else if (player.playerMap[i][j]== 6){
+					Monster monster = new Monster(monsterID, i , j);
+					// Robin muss den Construktor ändern // also einfach eine monsterID einfügen
+					monsterList[monsterID]= monster;
+					monsterID++;	
+				}
+				else if (player.playerMap[i][j]==4){
+					Healthpot healpot= new Healthpot(healpotID, i, j);
+					// muss ich in klasse healthpot ändern.
+					healpotList[healpotID] = healpot;
+					healpotID++;		
+				}
+				else if(player.playerMap[i][j] == 5){
+					Monster monster = new Monster(monsterID, i, j);
+					monsterList[monsterID]= monster;
+					monsterID++;
+			}
+				else if (player.playerMap[i][j]== 6){
+					doorx =j;
+					doory =i;
+				}
+		}
 		// Testmap
 		int[][] test = new int[15][15];
 
