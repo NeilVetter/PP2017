ea6e7eaf3f88f7439cf3608798d07f3add3fe83b
Neil <Neil@Rei.fritz.box>
2017-07-14 12:59:33 +0200

    abcd

--- src/pp2017/team20/server/ServerMain.java
+++ src/pp2017/team20/server/ServerMain.java
@@ -1,13 +1,14 @@
-package pp2017.team20.server.engine;
+package pp2017.team20.server;
+
+import pp2017.team20.server.engine.MessageProcessing;
 
 public class ServerMain {
+	public static void main(String[] args) {
 
-public static void main(String[] args){
-		
 		System.out.println("test");
-	
+
 		MessageProcessing messageProcessing = new MessageProcessing();
-		
+
 		messageProcessing.RecieveMessage();
 	}
 }
