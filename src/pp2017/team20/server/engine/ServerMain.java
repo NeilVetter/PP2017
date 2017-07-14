943961a8c9f7b0a0582ed85599959f362b102160
Neil <Neil@Rei.fritz.box>
2017-07-14 12:43:22 +0200

    Server Main Test

---
+++ src/pp2017/team20/server/engine/ServerMain.java
@@ -0,0 +1,13 @@
+package pp2017.team20.server.engine;
+
+public class ServerMain {
+
+public static void main(String[] args){
+		
+		System.out.println("test");
+	
+		MessageProcessing messageProcessing = new MessageProcessing();
+		
+		messageProcessing.RecieveMessage();
+	}
+}
