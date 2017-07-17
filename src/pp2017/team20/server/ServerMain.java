69fdc3a8f756785531733a20f3d7f623dabada38
Neil <Neil@Rei.fritz.box>
2017-07-17 22:11:36 +0200

    safc<

--- src/pp2017/team20/server/ServerMain.java
+++ src/pp2017/team20/server/ServerMain.java
@@ -1,9 +1,8 @@
-package server;
+package pp2017.team20.server;
 
 import pp2017.team20.server.engine.MessageProcessing;
 
 public class ServerMain {
-
 //	public static void main(String[] args) {
 //
 //		System.out.println("test");
