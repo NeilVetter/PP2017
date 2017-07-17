7968639435d33b2647ef78039a3a65688ea7e425
Yuxuan Kong <ypaikong@gmail.com>
2017-07-17 18:57:55 +0200

    herzschlagnachricht

---
+++ src/pp2017/team20/shared/Herzschlagnachricht.java
@@ -0,0 +1,21 @@
+package pp2017.team20.shared;
+
+/**
+ * Die Klasse fuer die Ich-bin-noch-da-Nachricht
+ * 
+ * @author Koruk, Samet, 5869110
+ */
+
+public class Herzschlagnachricht extends Message {
+
+	private static final long serialVersionUID = -3740108659792513974L;
+
+	public Herzschlagnachricht() {
+
+	}
+
+	public Herzschlagnachricht(int receiver) {
+		super.setReceiver(receiver);
+	}
+
+}
